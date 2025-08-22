a6be859976ee050a53d3b4a11d748dcf4aa9cdfa
VishalBhat07 <vishalbhat21092005@gmail.com>
2025-08-22 23:26:53 +0530

    feat: Add firebase functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,112 @@
-import './App.css'
-import Home from './pages/Home/Home'
-import About from "./pages/About/About";
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebaseFunctions/firebaseConfig";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
 import { Routes, Route, useNavigate } from "react-router-dom";
-
+import CropHealth from "./pages/CropHealth/CropHealth";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Schemes from "./pages/Schemes/Schemes";
+import PricePredictor from "./pages/Prediction/Predictor";
+import Login from "./pages/Login/Login";
+import FarmerProfile from "./pages/Profile/FarmerProfile";
+import BuyerProfile from "./pages/Profile/BuyerProfile";
+import LearningResourcesPage from "./pages/Learn/Learn";
+import { fetchFarmer } from "../firebaseFunctions/fetchUser";
+import FarmerMarket from "./pages/MarketPlace/FarmerMarket";
+import BuyerMarket from "./pages/MarketPlace/BuyerMarket";
+import PublicFarmerProfile from "./pages/Profile/PublicFarmerProfile";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop"; // Import ScrollToTop
+import CropHealthModal, {
+  useCropHealthModal,
+} from "./pages/CropHealth/CropHealth";
+import PlantAssistantButton from "./components/PlantAssistantButton/PlantAssistantButton";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [farmer, setFarmer] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const { isOpen, closeModal, openModal } = useCropHealthModal();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        try {
+          const fetchedFarmer = await fetchFarmer(user.email);
+          setFarmer(fetchedFarmer);
+          console.log(fetchedFarmer.farmerID);
+          setCurrentUser(user);
+        } catch (error) {
+          console.error("Error fetching farmer:", error);
+          setFarmer(null);
+        }
+      } else {
+        setCurrentUser(null);
+        setFarmer(null);
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (currentUser && !loading && window.location.pathname === "/login") {
+      const profilePath = farmer
+        ? `/profile/${farmer.farmerID}`
+        : `/profile/${currentUser.uid}`;
+      navigate(profilePath);
+    }
+  }, [currentUser, farmer, loading, navigate]);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-green-500"></div>
+      </div>
+    );
+  }
 
   return (
     <>
-{/* <Navbar farmer={farmer} /> */}
+      <Navbar farmer={farmer} />
       <div className="h-100vh">
+        {/* Integrate ScrollToTop */}
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/schemes" element={<Schemes />} />
+          <Route path="/test" element={<PricePredictor />} />
+          <Route path="/login" element={<Login />} />
+          <Route path={"/learn"} element={<LearningResourcesPage />} />
+          <Route
+            path="/profile/:userID"
+            element={farmer ? <FarmerProfile /> : <BuyerProfile />}
+          />
+
+          <Route
+            path={"/marketplace"}
+            element={
+              (farmer && <FarmerMarket farmerID={farmer.farmerID} />) || (
+                <BuyerMarket />
+              )
+            }
+          />
+          <Route path="farmer/:farmerID" element={<PublicFarmerProfile />} />
         </Routes>
       </div>
-      {/* <Footer /> */}
+      <div>
+        <PlantAssistantButton openModal={openModal} />
+        <CropHealthModal isOpen={isOpen} onClose={closeModal} />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
